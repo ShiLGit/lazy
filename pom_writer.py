4f3191ef5d3ac4d8d273b73f86075193a7d7cbcb
Lilian Shi <48463883+ShiLGit@users.noreply.github.com>
2025-03-03 11:12:28 -0500

    fix deleted xml decl on output file, refactor

--- pom_writer.py
+++ pom_writer.py
@@ -3,14 +3,12 @@ import json
 from io import StringIO
 import os
 import re
-proj_root = "."
 #this is a common fx. move to a util file later 
 def clean_xml_namespaces(root):
     for element in root.getiterator():
         if isinstance(element, etree._Comment):
             continue
         element.tag = etree.QName(element).localname
-    etree.cleanup_namespaces(root)
 
 # Given pom xml tree, return a map (property name -> val) of the <properties> section of pom
 def populate_properties_map (xml_root):
@@ -20,67 +18,92 @@ def populate_properties_map (xml_root):
         return map
     
     for p in props.iter(): 
-        map[p.tag] = p.text
+        map[p.tag] = {
+            'value': p.text,
+            'element': p
+        }
 
     del map['properties']
     return map
 
+def write_pom(xml, proj_root, fname = "pom_modified.xml"):
+    xml_declaration = ""
+    with open(proj_root + "/pom.xml") as fp:
+        xml_declaration = fp.readline()
 
-if __name__ == "__main__": 
+    with open(proj_root + f'/{fname}', "wb") as fp:
+        fp.write(xml_declaration.encode("utf-8"))
+        xml.write(fp)
+    print(f"[INFO] Changes written to modified file {proj_root}/{fname}")
+
+# Iterate through children of <dependencies> or similar; check if applicable rule applies for each child and make changes to file accordingly
+def processDeps(deps, rules, properties_map):
+    if len(deps) == 0:
+        print("[INFO] null element passed into deps! <dependencies> or <dependencyManagement> section is most likely missing")
+        return
+    for dep in deps: 
+            if dep.tag is etree.Comment:
+                continue 
+
+            # find groupId:artifactId of current dependency
+            grpId = dep.find("groupId")
+            if grpId is None: 
+                continue
+            grpId = grpId.text
+
+            artId = dep.find("artifactId")
+            if artId is None: 
+                continue 
+            artId = artId.text 
+
+            # does there exist a rule for current dependency? process if yes
+            matchingRule = rules.get(f"{grpId}:{artId}")
+            if matchingRule is not None: 
+                ver_tag = dep.find("version")
+                if ver_tag is None: 
+                    print(f"[WARNING] - No version listed for {grpId}:{artId} - skipping processing")
+                    continue
+
+                ver = ver_tag.text
+                ver_property = None
+                # Using a placeholder from properties or hardcoded?
+                if re.match(r"\s*\$\{.+\}", ver):
+                    ver_property = ver.replace("$", "").replace("{", "").replace("}", "")
+                    ver = properties_map.get(ver_property).get('value')
+
+                target_ver = ""
+                # find the range that version falls in
+                for rule in matchingRule: 
+                    lb, ub = rule['range'][0], rule['range'][1]
+                    under_ub = True if ub == "INF" else ver < ub 
+                    overeq_lb = True if lb == "INF" else ver >= lb
+                    if overeq_lb and under_ub:
+                        target_ver = rule['fixVersion']
+                        print(f"[INFO] GRADUATING {grpId}:{artId} FROM {ver} --> {target_ver}")
+                        if ver_property:
+                            props = properties_map[ver_property]['element']
+                            props.text = target_ver
+                        else: 
+                            print(f"[WARNING] {grpId}:{artId} has a hardcoded version!!! Consider using a property instead")
+                            ver_tag.text = target_ver
+                        break
+
+
+
+def main(proj_root = "."):
     fp = open("./config.json")
-    rules = json.load(fp)
+    rules = json.load(fp)["pom.xml"]
     fp.close()
     xml = etree.parse(proj_root + "/pom.xml")
-    clean_xml_namespaces(xml)
     xml_root = xml.getroot()
+    clean_xml_namespaces(xml)
+
     properties_map = populate_properties_map(xml_root)
     # check <dependencies>
-    deps = xml_root.find("dependencies") 
-    for dep in deps: 
-        if dep.tag is etree.Comment:
-            continue 
+    processDeps(xml_root.find("dependencies"), rules, properties_map)
+    processDeps(xml_root.find("dependencyManagement"), rules, properties_map)
 
-        grpId = dep.find("groupId")
-        if grpId is None: 
-            print("...? GroupId is none") 
-            continue
-        grpId = grpId.text
+    write_pom(xml, proj_root)
 
-        artId = dep.find("artifactId")
-        if artId is None: 
-            print("artId is none??")
-            continue 
-        artId = artId.text 
-
-        matchingRule = rules["pom.xml"].get(f"{grpId}:{artId}")
-        if matchingRule is not None: 
-            print("THERE IS A MATCH")
-            ver = dep.find("version")
-            if ver is None: 
-                # ver from depMgmt, parent?
-                continue
-
-            ver = ver.text
-            ver_property = None
-            # Using a placeholder from properties or hardcoded?
-            if re.match(r"\s*\$\{.+\}", ver):
-                ver_property = ver.replace("$", "").replace("{", "").replace("}", "")
-                ver = properties_map.get(ver_property)
-                
-            target_ver = ""
-            # find the range that version falls in
-            for rule in matchingRule: 
-                lb, ub = rule['range'][0], rule['range'][1]
-                under_ub = True if ub == "INF" else ver < ub 
-
-                if ver >= lb  and under_ub:
-                    target_ver = rule['fixVersion']
-                    print(f"GRADUATING {ver} --> {target_ver}")
-                    if ver_property:
-                        props = xml_root.find("properties")
-                        props.find(ver_property).text = target_ver
-                    break
-
-    with open("pom_modified.xml", "wb") as fp:
-        xml.write(fp)
-    #check <dependencyManagement>
+if __name__ == "__main__": 
+    main()
